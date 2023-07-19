2b5111b089dde01d8bf59a15522b50456460851f
Vignesh3223 <viketvicky@gmail.com>
2023-07-19 17:25:16 +0530

    19/07/23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,7 +44,18 @@ import { SortPipe } from 'src/shared/sort.pipe';
 import { FilterPipe } from '../shared/filter.pipe';
 //Search pipe
 import { SearchPipe } from '../shared/search.pipe';
+//Filter Status pipe
 import { FilterstatusPipe } from '../shared/filterstatus.pipe';
+//Material UI Modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCardModule } from '@angular/material/card';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -73,7 +84,16 @@ import { FilterstatusPipe } from '../shared/filterstatus.pipe';
     ToastModule,
     TabMenuModule,
     CardModule,
-    AvatarModule
+    AvatarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCardModule,
+    MatSortModule
   ],
   providers: [MessageService],
   bootstrap: [AppComponent]
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -12,10 +12,12 @@ import { Router } from '@angular/router';
 //User Interface
 import { User } from 'src/models/products';
 
+
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
 
